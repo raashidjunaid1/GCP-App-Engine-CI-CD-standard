8b1b5a9f5d165ff061d4d3c4f3a0928f0ed20f7d
raashidjunaid1 <raashidjunaid1@gmail.com>
2020-11-03 19:53:30 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const fastify = require('fastify')({
+    logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: " hello world"})
+})
+
+// Run the server!
+fastify.listen(process.env.PORT, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+})
