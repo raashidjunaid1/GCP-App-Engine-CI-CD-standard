a95466ac4b417ee6031354638459bd73598d02c7
raashidjunaid1 <raashidjunaid1@gmail.com>
2020-11-04 08:01:16 +0000

    converted to typescript

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const fastify = require('fastify')({
+    logger: true
+});
+// Declare a route
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: process.env.API_KEY });
+});
+// Run the server!
+fastify.listen(process.env.PORT, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    fastify.log.info(`server listening on ${address}`);
+});
+//# sourceMappingURL=index.js.map
